6261adab5710ea3b068600f849843a09f9f7d102
rcssntg <rcssntg@gmail.com>
2024-08-10 22:36:13 +0800

    update the run.bat path

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-cd "C:\Users\RCS\Documents\commitDaily\Daily-Git-Commit" & "C:/Users/RCS/AppData/Local/Programs/Python/Python312/python.exe" "C:/Users/RCS/Documents/commitDaily/Daily-Git-Commit/main.py"
+cd "C:\Users\RCS\Documents\commitDaily\Daily-Git-Commit" & "C:\Users\RCS\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\RCS\Documents\commitDaily\Daily-Git-Commit\main.py"
